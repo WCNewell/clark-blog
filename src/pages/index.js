4a8d4ba5882f16ebf0d3b7f3bfc79c5128a819b6
William Clark Newell <clark@clarknewell.tech>
2020-09-02 20:27:55 -0600

    Latest version of gatsby working fine
    Latest version of gatsby working fine

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ const IndexPage = () => (
     <>  
         <SEO title='Home' />
             <div>
-                <h1 className='main'>Hello. React hook demo for burger menu ala triple mode with Styled-Components and Sass.</h1>
+                
             </div>
     </>
 )
