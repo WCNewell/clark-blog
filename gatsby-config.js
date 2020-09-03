fc75832c518b21b30bcd8efea6e152845fbc1fcd
William Clark Newell <clark@clarknewell.tech>
2020-09-02 19:58:35 -0600

    Trying a slightly older version of gatsby
    Trying a slightly older version of gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,7 +23,7 @@ module.exports = {
                     wpcom_user: process.env.WP_USERNAME,
                     wpcom_pass: process.env.WP_PASSWORD,
                     },
-                verboseOutput: true,
+                verboseOutput: false,
             },
         },
         {
