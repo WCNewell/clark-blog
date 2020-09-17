95eba1a405f77a98d40377c8df916b2aa622e52d
William Clark Newell <clark@clarknewell.tech>
2020-09-17 16:32:24 -0600

    Re-implementing react-sticky-header
    Re-implementing react-sticky-header

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import PropTypes from "prop-types"
 import useDarkMode from 'use-dark-mode'
+import StickyHeader from 'react-sticky-header'
 import Logo from '../assets/clarklogo.inline.svg'
 import Nav from './nav'
 import RecentPosts from './recent-posts'
 import Footer from './footer'
 
 import StarLayout from './star-layout'
-import styled from 'styled-components'
 import '../styles/global-styles.scss'
 
 import SunIcon from '../assets/sun.inline.svg'
@@ -15,7 +15,7 @@ import MoonIcon from '../assets/moon.inline.svg'
 
 export const ThemeContext = React.createContext('day')
 
-const App = ({ children }) => {
+const App = () => {
     const [theme, setTheme] = React.useState('night')
     const darkMode = useDarkMode(false)
 
@@ -24,20 +24,20 @@ const App = ({ children }) => {
         theme === darkMode ? setTheme('night') : setTheme('day')
     }
   
-    return (
-        <ThemeContext.Provider value={'night'}>
-        <>
-            <Layout />
-                <Header>
-                    <Logo className='logo'/>
-                    <h1>clark newell</h1>
-                    <h2>web development + <a
+    const Header = () => (
+        <StickyHeader
+            header={
+                <div className="Header_root">
+                                        <Logo className='logo'/>
+
+        <h1 className="Header_title">clark newell</h1>
+        <h2>web development + <a
                         alt='link to Clark Newell fitness blog'
                         href='https://clarkfitness.netlify.com'>
                         fitness </a>
                         journey</h2>
-                    <Nav />
-                    <SunIcon    className='mode-icon'
+            
+            <SunIcon    className='mode-icon'
                                 onClick={() => {
                                     toggleTheme()
                                     darkMode.disable()
@@ -50,10 +50,21 @@ const App = ({ children }) => {
                                     darkMode.enable()
                                 }}
                                 alt='moon icon for dark star mode'
-                    />
-                </Header>
+            
+            />
+            <Nav />
+      </div>
+            }>
+                <section></section>
+        </StickyHeader>
+    )
+    
+    return (
+        <ThemeContext.Provider value={'night'}>
+        <>
+            <Layout />
+                <Header />
                 <RecentPosts />
-                    {children}
                 <Footer />
         </>
         </ThemeContext.Provider>
@@ -68,9 +79,6 @@ const Layout = () => {
                 theme === 'night' ? <StarLayout /> : null
         )
 }
-
-const Header = styled.div`
-`
 
 App.propTypes = {
     children: PropTypes.node.isRequired,
